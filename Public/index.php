55986ad360e1f0575d435b32701abd0b2269ac96
SungTri <sungofthelord204@gmail.com>
2025-12-07 15:19:44 +0700

    index

---
+++ Public/index.php
@@ -0,0 +1,10 @@
+<?php include '../../Config/db.php'; ?>
+<?php include '../../../Partials/header.php'; ?>
+<!-- Ảnh chính của trang chủ -->
+<div class="main-content">
+<div class="home-banner">
+    <img src="../Public/images/Trangchinh.jpg" alt="AMBASTYLE STORE">
+    <div class="banner-text">AMBASTYLE</div>
+</div>
+</div>
+<?php include '../../../Partials/footer.php'; ?>
